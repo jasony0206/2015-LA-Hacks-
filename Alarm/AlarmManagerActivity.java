bc28e79455261b9041b5107e55150fc7f9a3d85e
Sung Hyun (Jason) Yoon <jasony0206@gmail.com>
2015-04-04 11:26:12 -0700

    Create AlarmManagerActivity.java

---
+++ Alarm/AlarmManagerActivity.java
@@ -0,0 +1,68 @@
+package com.example.jason.alarm6;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AlarmManagerActivity extends Activity {
+
+    private AlarmManagerBroadcastReceiver alarm;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_manager);
+        alarm = new AlarmManagerBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    public void startRepeatingTimer(View view) {
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.SetAlarm(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void cancelRepeatingTimer(View view){
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.CancelAlarm(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onetimeTimer(View view){
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.setOnetimeTimer(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_alarm_manager, menu);
+        return true;
+    }
+}
