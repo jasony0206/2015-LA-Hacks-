252f8e8b6f6c80d1592e786cc7bee7548e73d780
eschiang <eschiang2@gmail.com>
2015-04-04 16:01:27 -0700

    Create weatherObject.java

---
+++ weather/weatherObject.java
@@ -0,0 +1,13 @@
+package weather;
+
+/**
+ * Created by Eric on 4/4/2015.
+ */
+public class WeatherObject {
+    public String name="";
+    public double temperature=0;
+    public String description="";
+    public int humidity=0;
+    public String icon="";
+    public byte[] iconData;
+}
