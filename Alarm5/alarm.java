5d4c6823015134672b3355f1c43fd555dfec3a53
Sung Hyun (Jason) Yoon <jasony0206@gmail.com>
2015-04-04 21:06:38 -0700

    Create alarm.java

---
+++ Alarm5/alarm.java
@@ -0,0 +1,255 @@
+package com.example.jason.alarm5;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+
+public class AlarmMainActivity extends Activity {
+
+    public class Alarm{
+        int aid;
+        Calendar cal;
+    };
+
+    public Alarm[] alarmArray;
+    private TimePicker timePicker;
+    private int index;
+    private int alarmID;
+    ListView listView;
+    public ArrayList<String> alarmString;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_alarm_main);
+        index = 0;
+        alarmArray = new Alarm[10];
+        alarmString = new ArrayList<String>();
+
+    }
+
+    public void daySelected(View view) {
+        Button b = (Button) view;
+        if(b.isActivated()) {
+            b.setTextColor(Color.parseColor("black"));
+            b.setBackgroundColor(Color.rgb(102, 204, 255));
+            b.setActivated(false);
+        }
+        else {
+            b.setTextColor(Color.parseColor("white"));
+            b.setBackgroundColor(Color.parseColor("blue"));
+            b.setActivated(true);
+        }
+    }
+
+    public void exitApp(View view) {
+        finish();
+    }
+
+    public void setAlarm(View view) {
+        //Create an offset from the current time in which the alarm will go off.
+        Calendar tempCal = Calendar.getInstance();
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        Calendar today = Calendar.getInstance();
+        int todayDay = (today).get(Calendar.DAY_OF_WEEK);
+
+        boolean sthClicked = false;
+        boolean nthClicked = false;
+
+        for(int i = 1; i < 8; i++){
+            today = Calendar.getInstance();
+            tempCal.set(Calendar.HOUR, timePicker.getCurrentHour());
+            tempCal.set(Calendar.MINUTE, timePicker.getCurrentMinute());
+            tempCal.set(Calendar.SECOND, 0);
+
+            if((i == 1 && !((Button) findViewById(R.id.sunday)).isActivated())
+                || (i == 2 && !((Button) findViewById(R.id.monday)).isActivated())
+                || (i == 3 && !((Button) findViewById(R.id.tuesday)).isActivated())
+                || (i == 4 && !((Button) findViewById(R.id.wednesday)).isActivated())
+                || (i == 5 && !((Button) findViewById(R.id.thursday)).isActivated())
+                || (i == 6 && !((Button) findViewById(R.id.friday)).isActivated())
+                || (i == 7 && !((Button) findViewById(R.id.saturday)).isActivated())
+                ) {
+                if (i == 7 && !sthClicked)
+                    nthClicked = true;
+                else
+                    continue;
+            }
+
+            sthClicked = true;
+            String weekday;
+
+
+            if(nthClicked) {
+                if (tempCal.compareTo(today) == 1){
+                    tempCal.add(Calendar.DAY_OF_MONTH, 1);
+                    switch (todayDay) {
+                        case 1:
+                            weekday = "Mon";
+                            break;
+                        case 2:
+                            weekday = "Tue";
+                            break;
+                        case 3:
+                            weekday = "Wed";
+                            break;
+                        case 4:
+                            weekday = "Thu";
+                            break;
+                        case 5:
+                            weekday = "Fri";
+                            break;
+                        case 6:
+                            weekday = "Sat";
+                            break;
+                        case 7:
+                            weekday = "Sun";
+                            break;
+                        default:
+                            weekday = "Wut";
+                    }
+                }
+                else {
+                    switch (todayDay) {
+                        case 1:
+                            weekday = "Sun";
+                            break;
+                        case 2:
+                            weekday = "Mon";
+                            break;
+                        case 3:
+                            weekday = "Tue";
+                            break;
+                        case 4:
+                            weekday = "Wed";
+                            break;
+                        case 5:
+                            weekday = "Thu";
+                            break;
+                        case 6:
+                            weekday = "Fri";
+                            break;
+                        case 7:
+                            weekday = "Sat";
+                            break;
+                        default:
+                            weekday = "Wut";
+                    }
+                }
+            }
+            else {
+                switch (i - todayDay) {
+                    case 1:
+                    case -6:
+                        //1day
+                        tempCal.add(Calendar.DAY_OF_MONTH, 1);
+                        break;
+                    case 2:
+                    case -5:
+                        //2day
+                        tempCal.add(Calendar.DAY_OF_MONTH, 2);
+                        break;
+                    case 3:
+                    case -4:
+                        tempCal.add(Calendar.DAY_OF_MONTH, 3);
+                        break;
+                    case 4:
+                    case -3:
+                        tempCal.add(Calendar.DAY_OF_MONTH, 4);
+                        break;
+                    case 5:
+                    case -2:
+                        tempCal.add(Calendar.DAY_OF_MONTH, 5);
+                        break;
+                    case 6:
+                    case -1:
+                        tempCal.add(Calendar.DAY_OF_MONTH, 6);
+                        break;
+                    case 0:
+                        if (today.compareTo(tempCal) == 1)
+                            tempCal.add(Calendar.DAY_OF_MONTH, 7);
+                        break;
+                }
+                switch (i) {
+                    case 1:
+                        weekday = "Sun";
+                        break;
+                    case 2:
+                        weekday = "Mon";
+                        break;
+                    case 3:
+                        weekday = "Tue";
+                        break;
+                    case 4:
+                        weekday = "Wed";
+                        break;
+                    case 5:
+                        weekday = "Thu";
+                        break;
+                    case 6:
+                        weekday = "Fri";
+                        break;
+                    case 7:
+                        weekday = "Sat";
+                        break;
+                    default:
+                        weekday = "Wut";
+                }
+            }
+
+
+            alarmArray[index] = new Alarm();
+            alarmArray[index].cal = tempCal;
+            alarmArray[index].aid = alarmID;
+
+            //Create a new PendingIntent and add it to the AlarmManager
+            Intent intent = new Intent(this, AlarmReceiverActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                    alarmID, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            AlarmManager am = (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+            am.set(AlarmManager.RTC_WAKEUP, tempCal.getTimeInMillis(), pendingIntent);
+
+            //initialize time string
+            String time =  Integer.toString(tempCal.get(Calendar.HOUR))
+                    + " : " + Integer.toString(tempCal.get(Calendar.MINUTE));
+            if(tempCal.get(Calendar.AM_PM) == 1)
+                time += " AM  ";
+            else
+                time += " PM  ";
+            time += weekday;
+            time += tempCal.get(Calendar.DAY_OF_MONTH);
+
+
+            alarmString.add(index, time);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, alarmString);
+            listView = (ListView) findViewById(R.id.list);
+            listView.setAdapter(adapter);
+
+            index++;
+            index %= 10;
+            alarmID++;
+        }
+
+    }
+}
